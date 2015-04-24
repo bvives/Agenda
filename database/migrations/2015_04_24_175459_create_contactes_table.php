def238c61e60d1bc9d5a93de2e5713f919925066
Bernat <bernatvivesgadea@gmail.com>
2015-04-24 20:12:51 +0200

    migrations

---
+++ database/migrations/2015_04_24_175459_create_contactes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('contactes', function(Blueprint $table)
+		{
+			$table->increments('id');
+                        $table->string('nom');
+                        $table->string('email');
+                        $table->string('tlf');
+			$table->timestamps();
+		});
+                Schema::create('cita_contacte', function(Blueprint $table)
+		{
+			$table->integer('cita_id');
+                        $table->integer('contacte_id');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('contactes');
+	}
+
+}
